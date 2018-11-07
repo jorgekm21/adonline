83562eca6837ba9823747b4047911a75faa37e02
Jorge Kassabji <jorgekm21@gmail.com>
2018-11-07 10:06:27 -0400

    Primer Commit
    Archivos iniciales para la presentacion del modulo

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'AdOnline Odoo',
+    'author': 'PCN & RP Suministros y Servicios Generales, C.A.',
+    'depends': ['base'],
+    'application': True,
+    'summary': 'Modulo AdOnline',
+    'version': '0.1a',
+    'website': 'http://pcnsuministros.com.ve',
+}
